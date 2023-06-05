5e386b70f88ff46c6b307786a268ed245b8e6930
Rozenrod <Rozenrod320@gmail.com>
2023-06-05 20:39:58 +0300

    Kaamira-2.0.10

--- src/lib/updateNotes.js
+++ src/lib/updateNotes.js
@@ -1,6 +1,19 @@
 updateNotes = [
   {
     "prefix":"Kaamira",
+    "version":"2.0.10",
+    "date":"5 June 2023",
+    "update":[
+      {
+				"type":"FIXED",
+        "text":"Исправлены ошибки.",
+			},{
+				"type":"FIXED",
+        "text":"Повышена стабильность.",
+			}
+    ]
+  },{
+    "prefix":"Kaamira",
     "version":"2.0.9",
     "date":"21 May 2023",
     "update":[
